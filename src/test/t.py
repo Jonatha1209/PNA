96810ae7e51f2f4604d6714a1ba6ae0ca4243e51
Jonatha1209 <147072651+Jonatha1209@users.noreply.github.com>
2025-04-05 14:25:18 +0900

    Update t.py

--- src/test/t.py
+++ src/test/t.py
@@ -2,8 +2,8 @@ import os
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from inter import Interprinter
-pna = Interprinter()
+from inter import Interpreter
+pna = Interpreter()
 current_dir = os.path.dirname(__file__)
 file_path = os.path.join(current_dir, "x.pna")
 pna.read(file_path)
